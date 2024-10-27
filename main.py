6dc765bb4d01016f67c89ba4000aba9afce6b11f
Surya Prakash <surya0prakash@proton.me>
2024-10-27 17:16:03 +0530

    added upload

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
-
-from flask import Flask
+from flask import Flask,request,jsonify
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+def handle_home():
+    return "Alive!"
+
+
+@app.route("/upload",methods=['POST','GET'])
+def handle_upload():
+    if(request.method != "POST"):
+        return jsonify({ "message":"Method Not Allowed"}) ,403
+    f = request.files['file']
+    f.save(f"./uploads/{secure_filename(f.filename if f.filename else 'test')}")
+
+    return jsonify({"message":"ok"}),200
 
 
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
+if [ ! -d "uploads" ]; then 
+	mkdir uploads
+fi
+
 python3 -m flask --app main run --debug
 
